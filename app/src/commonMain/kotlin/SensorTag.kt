74af38da0d41a123027b6ced60f34385df46ffab
Peter Bruinink <peter@plusot.com>
2024-10-23 16:25:51 +0200

    sensortag to nanosense

--- app/src/commonMain/kotlin/SensorTag.kt
+++ app/src/commonMain/kotlin/SensorTag.kt
@@ -3,11 +3,12 @@ package com.juul.sensortag
 import com.benasher44.uuid.Uuid
 import com.benasher44.uuid.uuidFrom
 import com.juul.kable.Bluetooth
+import com.juul.kable.Filter
 import com.juul.kable.Peripheral
 import com.juul.kable.Scanner
 import com.juul.kable.WriteType.WithResponse
 import com.juul.kable.characteristicOf
-import com.juul.kable.logs.Logging.Level.Events
+import com.juul.kable.logs.Logging.Level
 import com.juul.khronicle.Log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -15,46 +16,55 @@ import kotlin.text.HexFormat.Companion.UpperCase
 
 private const val GYRO_MULTIPLIER = 500f / 65536f
 
-val sensorTagUuid = uuidFrom("0000aa80-0000-1000-8000-00805f9b34fb")
-private val movementSensorServiceUuid = sensorTagUuid("aa80")
-private val movementSensorDataUuid = sensorTagUuid("aa81")
-private val movementNotificationUuid = sensorTagUuid("2902")
-private val movementConfigurationUuid = sensorTagUuid("aa82")
-private val movementPeriodUuid = sensorTagUuid("aa83")
+val sensorTagUuid = uuidFrom("19D0EA00-E8F2-537E-4F6C-D104768A1214".lowercase()) //0000aa80-0000-1000-8000-00805f9b34fb")
+private val nanoSenseServiceUuid = sensorTagUuid("EA00") //""aa80")
+private val nanoSenseColorCharacteristicUuid = sensorTagUuid("EA01") //""aa81")
+private val nanoSenseWeatherCharacteristicUuid = sensorTagUuid("2902")
+//private val movementConfigurationUuid = sensorTagUuid("aa82")
+//private val movementPeriodUuid = sensorTagUuid("aa83")
 private val clientCharacteristicConfigUuid = Bluetooth.BaseUuid + 0x2902
 
-private val movementConfigCharacteristic = characteristicOf(
-    service = movementSensorServiceUuid,
-    characteristic = movementConfigurationUuid,
+private val colorCharacteristic = characteristicOf(
+    service = nanoSenseServiceUuid,
+    characteristic = nanoSenseColorCharacteristicUuid,
 )
 
-private val movementDataCharacteristic = characteristicOf(
-    service = movementSensorServiceUuid,
-    characteristic = movementSensorDataUuid,
+private val weatherCharacteristic = characteristicOf(
+    service = nanoSenseServiceUuid,
+    characteristic = nanoSenseWeatherCharacteristicUuid,
 )
+//private val movementDataCharacteristic = characteristicOf(
+//    service = movementSensorServiceUuid,
+//    characteristic = movementSensorDataUuid,
+//)
 
-private val movementPeriodCharacteristic = characteristicOf(
-    service = movementSensorServiceUuid,
-    characteristic = movementPeriodUuid,
-)
+//private val movementPeriodCharacteristic = characteristicOf(
+//    service = movementSensorServiceUuid,
+//    characteristic = movementPeriodUuid,
+//)
 
 val scanner = Scanner {
     logging {
-        level = Events
+        level = Level.Data //Events
     }
     filters {
         match {
-            services = listOf(sensorTagUuid)
+            name = Filter.Name.Exact("NanoSense") //= listOf(sensorTagUuid)
         }
     }
 }
 
 val services = listOf(
-    movementSensorServiceUuid,
-    movementSensorDataUuid,
-    movementNotificationUuid,
-    movementConfigurationUuid,
-    movementPeriodUuid,
+
+    nanoSenseServiceUuid,
+    nanoSenseColorCharacteristicUuid,
+    nanoSenseWeatherCharacteristicUuid,
+
+//    movementSensorServiceUuid,
+//    movementSensorDataUuid,
+//    movementNotificationUuid,
+//    movementConfigurationUuid,
+//    movementPeriodUuid,
     clientCharacteristicConfigUuid,
 )
 
@@ -63,7 +73,7 @@ class SensorTag(
 ) : Peripheral by peripheral {
 
     val gyro: Flow<Vector3f> = peripheral
-        .observe(movementDataCharacteristic)
+        .observe(colorCharacteristic)
         .map(::Vector3f)
         .map { it * GYRO_MULTIPLIER }
 
@@ -75,30 +85,32 @@ class SensorTag(
         val data = byteArrayOf(value.toByte())
 
         Log.verbose { "Writing gyro period" }
-        peripheral.write(movementPeriodCharacteristic, data, WithResponse)
+        //peripheral.write(movementPeriodCharacteristic, data, WithResponse)
         Log.info { "Writing gyro period complete" }
     }
 
     /** Period (in milliseconds) within the range 100-2550 ms. */
     suspend fun readGyroPeriod(): Int {
-        val value = peripheral.read(movementPeriodCharacteristic)
-        Log.info { "movement → readPeriod → value = ${value.toHexString(UpperCase)}" }
-        return value[0].toInt() and 0xFF * 10
+        //val value = peripheral.read(movementPeriodCharacteristic)
+        Log.info { "movement → readPeriod → value = UNKNOWN" } //${value.toHexString(UpperCase)}" }
+        //return value[0].toInt() and 0xFF * 10
+        return 0
     }
 
     suspend fun enableGyro() {
         Log.info { "Enabling gyro" }
-        peripheral.write(movementConfigCharacteristic, byteArrayOf(0x7F, 0x0), WithResponse)
+        //peripheral.write(movementConfigCharacteristic, byteArrayOf(0x7F, 0x0), WithResponse)
         Log.info { "Gyro enabled" }
     }
 
     suspend fun disableGyro() {
-        peripheral.write(movementConfigCharacteristic, byteArrayOf(0x0, 0x0), WithResponse)
+        //peripheral.write(movementConfigCharacteristic, byteArrayOf(0x0, 0x0), WithResponse)
     }
 }
 
 private fun sensorTagUuid(short16BitUuid: String): Uuid =
-    uuidFrom("f000${short16BitUuid.lowercase()}-0451-4000-b000-000000000000")
+    //uuidFrom("f000${short16BitUuid.lowercase()}-0451-4000-b000-000000000000")
+    uuidFrom("19D0${short16BitUuid}-E8F2-537E-4F6C-D104768A1214".lowercase())
 
 private fun characteristicOf(service: Uuid, characteristic: Uuid) =
     characteristicOf(service.toString(), characteristic.toString())
